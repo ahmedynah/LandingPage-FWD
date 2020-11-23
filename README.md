c898b6fb56521f1ddb718694deeb70652f672bb3
Ahmed Abouelnasr <ahmedhanyhasn@yahoo.com>
2020-11-23 17:09:36 +0200

    -m "first Submission"

--- README.md
+++ README.md
@@ -1,14 +1,24 @@
 # Landing Page Project
 
 ## Table of Contents
+1- Introduction
+2- Architecture
 
-* [Instructions](#instructions)
+## 1- Introduction
 
-## Instructions
+This project is to implement the knowledge gained in manipulating the DOM and to make the frontend more alive.
 
-The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+## 2- Architecture 
 
-To get started, open `js/app.js` and start building out the app's functionality
+    The folder structure:
+    css
+    - styles.css    
+    index.html
+    js
+    - app.js
+    README.md
 
-For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+
+
+
 # LandingPage-FWD
--- js/app.js
+++ js/app.js
@@ -17,13 +17,22 @@
  * Define Global Variables
  * 
 */
-
-
+const frag = document.createDocumentFragment();
+const sections = document.getElementsByTagName("section");
+const main = document.getElementsByTagName("main");
+const list = document.querySelector("#navbar__list");
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
+function isInTopOfViewPort(elem){
+    
+    let topDis = elem.getBoundingClientRect();
+    return (
+        topDis.top >= 0 &&
+        topDis.top <= ((window.innerHeight || document.documentElement.clientHeight) / 4));
+}
 
 
 
@@ -34,24 +43,69 @@
 */
 
 // build the nav
+function build_Nav(){
+    
+        for(section of sections){
+            let anchor = document.createElement("a");
+            //anchor.href = "#" + section.id;
+            anchor.classList.add("menu__link");
+            anchor.innerHTML = section.getAttribute("data-nav");
+            frag.appendChild(anchor);
+        }
+        
+        list.innerHTML = "";
+        list.appendChild(frag);
+        list.style.display = "none";
+    }
 
 
 // Add class 'active' to section when near top of viewport
+function add_Active(){
+    
+    list.style.display = "block";
+    for(section of sections){
+        if(isInTopOfViewPort(section)){
+            !section.classList.contains("your-active-class")  && section.classList.add("your-active-class");
+        }
+        else{
+            section.classList.contains("your-active-class") && section.classList.remove("your-active-class");
+        }
+    }
 
+    setTimeout(()=>{
+        list.style.display = "none";
+    }, 3000);  
+
+}
 
 // Scroll to anchor ID using scrollTO event
+function goto_section(event){
 
-
+    let wantedSection = event.target.textContent;
+    let found = undefined;   
+   
+    for(section of sections){ 
+        (section.getAttribute("data-nav") == wantedSection) &&  (found = section);
+        }
+                  
+    window.scrollTo({
+            top: found.offsetTop - 100,
+            behavior: 'smooth',
+     })   
+            
+}
 /**
  * End Main Functions
  * Begin Events
  * 
 */
 
-// Build menu 
+// Build menu
+ document.addEventListener("DOMContentLoaded", build_Nav);
 
 // Scroll to section on link click
+list.addEventListener("click",goto_section);
 
 // Set sections as active
-
+window.addEventListener("scroll",add_Active);
 
